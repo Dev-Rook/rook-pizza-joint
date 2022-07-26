7de6a06b8bc7965a4b3f313fc7415a51cea899b4
Doc Savage <97268006+DocSavage11@users.noreply.github.com>
2022-07-26 18:04:42 -0500

    Upload

---
+++ pizzajoint/src/components/Home.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import {motion} from 'framer-motion'
+
+const Home = () => {
+  return (
+    <motion.div className="home container"
+      initial={{opacity: 0}}
+      animate={{opacity: 1}}
+      transition={{delay: 1.5, duration: 1.5}}
+    >
+      <motion.h2 animate={{}}>
+        Welcome to Pizza Joint
+      </motion.h2>
+      <Link to="/base">
+        <motion.button
+          animate={{}}
+        >
+          Create Your Pizza
+        </motion.button>
+      </Link>
+    </motion.div>
+  )
+}
+
+export default Home;

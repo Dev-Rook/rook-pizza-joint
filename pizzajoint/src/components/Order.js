c8683878b23934447e781ff5a520e652f13420b2
Doc Savage <97268006+DocSavage11@users.noreply.github.com>
2022-07-27 23:11:53 -0500

    Animate Presence

--- pizzajoint/src/components/Order.js
+++ pizzajoint/src/components/Order.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import { motion } from 'framer-motion';
+import React, {useState} from 'react';
+import { motion, AnimatePresence } from 'framer-motion';
 
 
 const containerVariants = {
@@ -35,13 +35,28 @@ const childVariants = {
 
 
 const Order = ({ pizza }) => {
+
+  const [showTitle, setShowTitle] = useState(true);
+
+  setTimeout(() => {
+    setShowTitle(false);
+  }, 4000)
+
   return (
     <motion.div className="container order"
       variants={containerVariants}
       initial="hidden"
       animate="visible"
     >
-      <h2>Thank you for your order :)</h2>
+      <AnimatePresence>
+        {showTitle && (
+          <motion.h2
+            exit={{y: -1000}}
+          >
+            Thank you for your order :)
+            </motion.h2>
+        )}
+      </AnimatePresence>
       <motion.p
         variants={childVariants}
       >

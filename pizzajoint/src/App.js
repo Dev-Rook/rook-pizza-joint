08e4fed9d3ae0de985d8d153264fc1a084348c35
Doc Savage <97268006+DocSavage11@users.noreply.github.com>
2022-07-27 23:37:58 -0500

    Modal is set

--- pizzajoint/src/App.js
+++ pizzajoint/src/App.js
@@ -8,11 +8,13 @@ import Home from './components/Home';
 import Base from './components/Base';
 import Toppings from './components/Toppings';
 import Order from './components/Order';
+import Modal from './components/Modal';
 
 
 function App() {
   const location = useLocation();
 
+  const [showModal, setShowModal] = useState(false);
 
   const [pizza, setPizza] = useState({ base: "", toppings: [] });
 
@@ -33,6 +35,8 @@ function App() {
   return (
     <>
       <Header />
+      <Modal showModal={showModal} setShowModal={setShowModal} />
+
 
       <AnimatePresence exitBeforeEnter>
         <Switch location={location} key={location.key}>
@@ -43,7 +47,7 @@ function App() {
             <Toppings addTopping={addTopping} pizza={pizza} />
           </Route>
           <Route path="/order">
-            <Order pizza={pizza} />
+            <Order pizza={pizza} setShowModal={setShowModal} />
           </Route>
           <Route path="/">
             <Home />
